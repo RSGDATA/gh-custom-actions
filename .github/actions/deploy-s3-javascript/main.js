e0effad83b4cadf0ae1202e4fec2e7e5afee6b9c
gitactions <rgviolin1234@gmail.com>
2026-04-23 20:52:40 -0500

    downgrade @actions/* to CommonJS-compatible versions and fix notice typo

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.cotice('Hello from my custom Javascript action!');
+  core.notice('Hello from my custom Javascript action!');
 }
 
 run();
